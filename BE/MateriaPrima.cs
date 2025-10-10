6cf0e9d07bab5315c87d935d79735c2c734503bb
joaqui-bit <joaquimidon@gmail.com>
2025-10-10 17:51:11 +0000

    new class materias primas

---
+++ BE/MateriaPrima.cs
@@ -0,0 +1,26 @@
+namespace BE
+{
+    ///Para subir cambios a git
+    /// Git Add .
+    /// Git Commit -m " NOMBRE DEL CAMBIO "
+    /// Git Push
+    public class MateriasPrima
+    {
+        MateriasPrima(int x) { ID = x;}
+
+        MateriasPrima(object[] pItemArray, Proveedores P)
+        {
+            ID = Convert.ToInt32(pItemArray[0]);
+            Nombre = pItemArray[1].ToString();
+            CantStock = Convert.ToInt32(pItemArray[2]);
+            CostoUni = Convert.ToInt32(pItemArray[3]);
+            Prove = P;
+        }
+
+        public int ID {get; set;}
+        public string Nombre {get;set;}
+        public int CantStock {get;set;}
+        public int CostoUni {get;set;}
+        public Proveedores Prove {get;set;}
+    }
+}
