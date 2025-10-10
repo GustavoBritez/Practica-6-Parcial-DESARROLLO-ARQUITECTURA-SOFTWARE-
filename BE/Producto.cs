b9497417c9f42081d2106882ef17d3925ed3ce04
joaqui-bit <joaquimidon@gmail.com>
2025-10-10 19:10:04 +0000

    guardo seguridad

---
+++ BE/Producto.cs
@@ -0,0 +1,24 @@
+namespace BE
+{
+    ///Para subir cambios a git
+    /// Git Add .
+    /// Git Commit -m " NOMBRE DEL CAMBIO "
+    /// Git Push
+    public class Producto
+    {
+        public Producto(){}
+
+        public Producto(object[] pItemArray)
+        {
+            ID = Convert.Toint32(pItemArray[0]);
+            Nombre = pItemArray[1].ToString();
+            PrecioUniVenta = Convert.ToDecimal(pItemArray[2]);
+            CantStock = Convert.ToDecimal(pItemArray[3]);
+        }
+
+        public int id {get;set;}
+        public string Nombre {get;set;}
+        public decimal PrecioUniVenta {get;set;}
+        public decimal CantStock {get;set;}
+    }
+}
