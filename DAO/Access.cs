b8cc4a59aabbedd108fac5c054af6fee77e84557
Gustavo Daniel Britez Alvarenga <115171270+GustavoBritez@users.noreply.github.com>
2025-10-10 11:44:11 -0300

    Carga DAO

---
+++ DAO/Access.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class Access
+    {
+        public SqlConnection conn = new("AGREGAR DATASOURCE BD");
+        public SqlTransaction transaction;
+
+
+        public void Open()
+        {
+            conn.Open();
+        }
+        public void Close()
+        {
+            conn.Close();
+        }
+        public void Start_TX()
+        {
+            transaction = conn.BeginTransaction();
+        }
+        public void Stop_TX()
+        {
+            transaction.Rollback();
+        }
+        public void Commit_TX()
+        {
+            transaction.Commit();
+        }
+        public void Escribir( string Query , SqlParameter[] sp = null )
+        {
+            try
+            {
+                Open();
+                using ( SqlCommand cmd = new SqlCommand(Query, conn)) 
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    
+                    if ( sp is not null )
+                        cmd.Parameters.AddRange( sp );
+
+                    Start_TX();
+                    cmd.Transaction = transaction;
+                    int resultado = cmd.ExecuteNonQuery();
+                    Commit_TX();
+
+                }
+            }
+            catch( Exception ex )
+            {
+                throw new ArgumentException(ex.Message);
+            }
+        }
+        public DataTable Leer( string Query , SqlParameter[] sp )
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                Open();
+                using (SqlCommand cmd = new SqlCommand(Query, conn))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    if (sp is not null)
+                        cmd.Parameters.AddRange(sp);
+
+                    using (SqlDataAdapter adapter = new(cmd))
+                    {
+                        adapter.Fill(dt);
+                    }
+                }
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+        }
+    }
+}
