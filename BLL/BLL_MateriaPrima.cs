9bcae12d94634909af152dbd112536dec8521e50
joaqui-bit <joaquimidon@gmail.com>
2025-10-13 19:02:45 +0000

    se agrago BLL y ORM MateriaPrima

---
+++ BLL/BLL_MateriaPrima.cs
@@ -0,0 +1,20 @@
+﻿namespace BLL
+{
+    public class BLL_MateriaPrima : Iabmc<MateriaPrima> 
+    {
+        ORM_MateriaPrima MP;
+
+        public BLL_MateriaPrima()
+        { MP = new ORM_MateriaPrima(); }
+
+        public void Alta(MateriaPrima mp)
+        {
+            MP.Alta(mp);
+        }
+
+        public void Baja(MateriaPrima mp)
+        {
+            MP.Baja(mp);
+        }
+    }
+}
---
+++ ORM/ORM_MateriaPrima.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+
+namespace ORM_MateriaPrima
+{
+    public class ORM_MateriaPrima
+    {
+        DataSet DS; 
+
+        public ORM_MateriaPrima() { }
+
+        public void Alta(MateriaPrima mp)
+        {
+            try
+            {
+
+            }
+            catch (exception ex) { throw ex; }
+        }
+        
+        public void Baja(ORM_MateriaPrima mp)
+        {
+            try
+            {
+
+            }
+            catch (exception ex) { throw ex; }
+        }
+    }
+}
