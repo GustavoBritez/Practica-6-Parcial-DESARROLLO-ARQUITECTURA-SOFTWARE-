e2241d41f89d935c57582de0c3ffd6ef6a01c141
Joaquin Midón <Joaquin.Midon@alumnos.uai.edu.ar>
2025-10-25 10:57:26 -0300

    cambios en gui de parte de joaquin

--- Syncfusion/obj/Debug/net9.0/Syncfusion.AssemblyInfo.cs
+++ Syncfusion/obj/Debug/net9.0/Syncfusion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Syncfusion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba2d72417ce2468830b5fae8a9c88d95abe2260a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e62cad52357aacd871221da8d5573913f0f2731a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Syncfusion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Syncfusion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
