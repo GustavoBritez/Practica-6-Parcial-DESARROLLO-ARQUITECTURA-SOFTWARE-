ca9573d0066c75c7650f0bd9a337a19fcc6b099e
Gus <danielgus@outlook.es>
2026-03-25 21:52:06 -0300

    Agregamos una BE

--- BE/HabitacionBE.cs
+++ BE/HabitacionBE.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BE
 {
-    internal class HabitacionBE
+    public class HabitacionBE
     {
+        private int numero;
+        private double precioBase;
+        private int capacidad;
+        private bool disponibilidad;
+
+        public int Numero { get { return numero; } set { numero = value; } }
+        public double PrecioBase { get { return precioBase; } set { precioBase = value; } }
+        public int Capacidad { get { return capacidad; } set { capacidad = value; } }
+        public bool Disponibilidad { get {return disponibilidad;} set { disponibilidad = value;} }
+
     }
 }
