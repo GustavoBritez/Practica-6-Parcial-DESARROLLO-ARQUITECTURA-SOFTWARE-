395f697ae59e250fea4f2b22bdebe4684a73eede
Gus <danielgus@outlook.es>
2026-03-25 23:00:24 -0300

    debemos hacer la DAL

--- BE/HabitacionBE.cs
+++ BE/HabitacionBE.cs
@@ -10,14 +10,22 @@ namespace BE
 {
     public class HabitacionBE
     {
+
         private int numero;
         private double precioBase;
         private int capacidad;
         private bool disponibilidad;
 
+        [Key]
         public int Numero { get { return numero; } set { numero = value; } }
+
+        [Range(0.1, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
         public double PrecioBase { get { return precioBase; } set { precioBase = value; } }
+
+        [Range(1, int.MaxValue, ErrorMessage = "La capacidad debe ser mayor a 0")]
         public int Capacidad { get { return capacidad; } set { capacidad = value; } }
+
+        [Required]
         public bool Disponibilidad { get {return disponibilidad;} set { disponibilidad = value;} }
 
     }
